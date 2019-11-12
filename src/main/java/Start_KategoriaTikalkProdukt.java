e4291473cb8a7e804a58b7dfbf1aff692c656d63
mateu <mateusz.bialos@gmail.com>
2019-11-12 13:23:18 +0100

    Dodano kilka produktow dla sprawdzenia czy wszystko dziala - OK.

--- src/main/java/Start_KategoriaTikalkProdukt.java
+++ src/main/java/Start_KategoriaTikalkProdukt.java
@@ -10,7 +10,7 @@ public class Start_KategoriaTikalkProdukt {
         Session session = HibernateUtil.getSessionFacotory().openSession();
         session.beginTransaction();
 
-        //        główne
+//        główne
         KategoriaTikalk root = new KategoriaTikalk();
         root.setOpis("root");
 
@@ -18,14 +18,26 @@ public class Start_KategoriaTikalkProdukt {
         KategoriaTikalk motoryzajca = new KategoriaTikalk();
         motoryzajca.setOpis("motoryzacja");
 
-//        2 stopień
+//        2.1 stopień
         KategoriaTikalk osobowe = new KategoriaTikalk();
         osobowe.setOpis("osobowe");
 
+        //        2.2 stopień
+        KategoriaTikalk ciezarowe = new KategoriaTikalk();
+        ciezarowe.setOpis("ciezarowe");
 
-        //  3 stopień
+
+        //  3.1 stopień -sobowe
         KategoriaTikalk mercedes = new KategoriaTikalk();
         mercedes.setOpis("mercedes");
+
+        //  3.2 stopień -sobowe
+        KategoriaTikalk bmw = new KategoriaTikalk();
+        bmw.setOpis("bmw");
+//      3.3 stopień - ciężarowe
+
+        KategoriaTikalk man = new KategoriaTikalk();
+        man.setOpis("Ciezarowy MAN");
 
 //        ustawiamy zależności drzewa
         List<KategoriaTikalk> rootChildren = root.getChildren();
@@ -33,35 +45,114 @@ public class Start_KategoriaTikalkProdukt {
 
         List<KategoriaTikalk> motoryzacjaChildren = motoryzajca.getChildren();
         motoryzacjaChildren.add(osobowe);
+        motoryzacjaChildren.add(ciezarowe);
         motoryzajca.setParent(root);
 
         List<KategoriaTikalk> osoboweChildren = osobowe.getChildren();
         osoboweChildren.add(mercedes);
+        osoboweChildren.add(bmw);
         osobowe.setParent(motoryzajca);
 
         mercedes.setParent(osobowe);
-// tworzę produkty
-        Produkt mercedesAklasa = new Produkt();
-        mercedesAklasa.setTytul("Mercedes W169");
-        mercedesAklasa.setOpis("Piękny baby-Benz");
-        mercedesAklasa.setTypProduktu(TypProduktu.SPORT);
-        mercedesAklasa.setKategoriaTikalk(mercedes);
+        bmw.setParent(osobowe);
 
-        Produkt mercedesSklasa = new Produkt();
-        mercedesSklasa.setTytul("Mercedes W410");
-        mercedesSklasa.setOpis("Dostojna limuzyna");
-        mercedesSklasa.setTypProduktu(TypProduktu.MOTO);
-        mercedesSklasa.setKategoriaTikalk(mercedes);
+        List<KategoriaTikalk> ciezaroweChildren = ciezarowe.getChildren();
+        ciezaroweChildren.add(man);
+        ciezarowe.setParent(motoryzajca);
 
-//        utrwalam
+        man.setParent(ciezarowe);
+
+        // utrwalamy drzewo zależności
+
+
+//        tworzę klika produktów (merc-os, bmw-os, cieżarowy-MAN)
+        Produkt cKlasa = new Produkt();
+        Produkt sKlasa = new Produkt();
+        Produkt glaKlasa = new Produkt();
+        Produkt eKlasa = new Produkt();
+
+        Produkt x1 = new Produkt();
+        Produkt x3 = new Produkt();
+        Produkt x5 = new Produkt();
+
+        Produkt g90 = new Produkt();
+        Produkt tgl = new Produkt();
+        Produkt tgm = new Produkt();
+
+        cKlasa.setKategoriaTikalk(mercedes);
+        cKlasa.setTypProduktu(TypProduktu.MOTO);
+        cKlasa.setTytul("Mercedes C-klass");
+        cKlasa.setOpis("Piękny, nowy, nie śmigany");
+
+        eKlasa.setKategoriaTikalk(mercedes);
+        eKlasa.setTypProduktu(TypProduktu.MOTO);
+        eKlasa.setTytul("Mercedes E-klass");
+        eKlasa.setOpis("Nowoczesna limuzyna dla każdego boagacza");
+
+        sKlasa.setKategoriaTikalk(mercedes);
+        sKlasa.setTypProduktu(TypProduktu.MOTO);
+        sKlasa.setTytul("Mercedes S-klass");
+        sKlasa.setOpis("Zadbany, nie śmigany, wcześniejszy kierowca często go mył");
+
+        glaKlasa.setKategoriaTikalk(mercedes);
+        glaKlasa.setTypProduktu(TypProduktu.MOTO);
+        glaKlasa.setTytul("Mercedes G-klass");
+        glaKlasa.setOpis("Miejska terenówka.");
+
+        x1.setKategoriaTikalk(bmw);
+        x1.setTypProduktu(TypProduktu.MOTO);
+        x1.setTytul("BMW x1");
+        x1.setOpis("Jako 3 auto w rodzinie.");
+
+        x3.setKategoriaTikalk(bmw);
+        x3.setTypProduktu(TypProduktu.MOTO);
+        x3.setTytul("BMW x3");
+        x3.setOpis("Doskonały do jazdy po kiełbasę na grilla do Biedronki");
+
+        x3.setKategoriaTikalk(bmw);
+        x3.setTypProduktu(TypProduktu.MOTO);
+        x3.setTytul("BMW x5");
+        x3.setOpis("Ladny, przestronny, drogi. Wszystko czego pragniesz!");
+
+        g90.setKategoriaTikalk(man);
+        g90.setTypProduktu(TypProduktu.MOTO);
+        g90.setTytul("MAN G90");
+        g90.setOpis("Mały przebieg");
+
+        tgl.setKategoriaTikalk(man);
+        tgl.setTypProduktu(TypProduktu.MOTO);
+        tgl.setTytul("MAN TGL");
+        tgl.setOpis("Piękny lakier");
+
+        tgm.setKategoriaTikalk(man);
+        tgm.setTypProduktu(TypProduktu.MOTO);
+        tgm.setTytul("MAN TGM");
+        tgm.setOpis("Wygodne fotele");
 
         session.persist(root);
-        session.persist(motoryzajca);
-        session.persist(osobowe);
-        session.persist(mercedes);
 
-        session.persist(mercedesAklasa);
-        session.persist(mercedesSklasa);
+        session.persist(motoryzajca);
+
+        session.persist(osobowe);
+        session.persist(ciezarowe);
+
+        session.persist(mercedes);
+        session.persist(bmw);
+
+        session.persist(man);
+
+        session.persist(cKlasa);
+        session.persist(eKlasa);
+        session.persist(sKlasa);
+        session.persist(glaKlasa);
+
+        session.persist(x1);
+        session.persist(x3);
+        session.persist(x5);
+
+        session.persist(g90);
+        session.persist(tgl);
+        session.persist(tgm);
 
         session.flush();
         session.close();
